2dece118231c38b23981ee097aee85a7dcc8ae1b
Shahid <rwtc66@gmail.com>
2021-05-07 13:37:30 +0530

    cors issue

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const nconf = require("nconf");
 const bodyParser = require("body-parser");
 const chalk = require("chalk");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 
 // load config file
@@ -13,6 +14,7 @@ nconf
     file: __dirname + "/config.json",
   });
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
